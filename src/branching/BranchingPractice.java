769417df142a6e74508c7c08d07bfe22a7022552
Hussain Rezai <hussain.rasa111@gmail.com>
2022-11-10 07:57:33 -0500

    first commit

---
+++ src/branching/BranchingPractice.java
@@ -0,0 +1,12 @@
+package branching;
+
+public class BranchingPractice {
+
+	public static void main(String[] args) {
+		System.out.println("HI, this is the first branching practice! ");
+	}
+	
+	public static void showMyBranch() {
+		System.out.println("This is your branch!");
+	}
+}
